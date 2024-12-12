2c860fbc8f108cc8a21be9914e13e8a18ff451d9
Domenica Rodriguez <112053642+DomeLiz@users.noreply.github.com>
2024-12-12 05:21:57 +0100

    despues de eliminar persons

--- backend/src/db/models/index.js
+++ backend/src/db/models/index.js
@@ -1,13 +1,10 @@
 //llamara a todos los modelos 
-const { Person, PersonSchema } = require ('./persons.model');
 
 const { User, UserSchema } = require('./users.model'); 
 const { Usuario, UsuarioSchema } = require('./usuario.model');
 const { OTP, OTPSchema } = require('./OTP');
 
-
 function setupModels(sequelize) {
-    Person.init(PersonSchema,Person.config(sequelize));
     User.init(UserSchema, User.config(sequelize)); // Configura el modelo de User
     Usuario.init(UsuarioSchema, Usuario.config(sequelize));
     OTP.init(OTPSchema, OTP.config(sequelize)); 
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const personsRouter = require('./persons.routes');
 const authRouter = require('./auth.routes'); 
 const usuarioRouter = require('./usuario.routes'); 
 
 function routerApi(app) {
     const router = express.Router();
     app.use('/api/v1', router);
-    router.use('/persons', personsRouter);
     router.use('/auth', authRouter); 
     router.use('/usuarios', usuarioRouter);
 }
